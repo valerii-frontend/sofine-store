0feb05a7620fa4c109b7a055cb49a2a339c1da8c
valerii <lvaldev.code@gmail.com>
2022-12-21 23:20:09 +0100

    add slider controls style

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,20 +1,56 @@
 import slide01 from "../../assets/images/slide1-big.jpg";
 import slide02 from "../../assets/images/slide2-big.jpg";
+import slide03 from "../../assets/images/slide3-big.jpg";
 
-import styles from "./Slider.module.css";
+import { Pagination, Navigation, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "./styles.css";
 
 export default function Slider() {
   return (
-    <section className={styles.home}>
-      <div className={styles.slide}>
-        <h1>o1. </h1>
-        <h2>Bez kompromisu.</h2>
-        <h2>Idealnie takie jak lubisz</h2>
-        <h3>
-          keune <span>design</span>
-        </h3>
+    <Swiper
+      modules={[Pagination, Navigation, Autoplay]}
+      loop={true}
+      navigation={true}
+      autoplay={true}
+      pagination={{
+        clickable: true,
+      }}>
+      <SwiperSlide>
         <img src={slide01} alt='woman with light hair' />
-      </div>
-    </section>
+        <div className='content'>
+          <h1>o1. </h1>
+          <h2>Bez kompromisu.</h2>
+          <h2>Idealnie takie jak lubisz</h2>
+          <h3>
+            keune <span>design</span>
+          </h3>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide02} alt='man in white shirt' />
+        <div className='content'>
+          <h1>o1. </h1>
+          <h2>Bez kompromisu.</h2>
+          <h2>Idealnie takie jak lubisz</h2>
+          <h3>
+            keune <span>design</span>
+          </h3>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide03} alt='man in white shirt' />
+        <div className='content'>
+          <h1>o1. </h1>
+          <h2>Bez kompromisu.</h2>
+          <h2>Idealnie takie jak lubisz</h2>
+          <h3>
+            keune <span>design</span>
+          </h3>
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
 }
