94fabfbc4e93710d0ef9a3aab3ced34d676fdff6
valerii <lvaldev.code@gmail.com>
2022-12-21 21:21:32 +0100

    add rwd header

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./Navigation.module.css";
@@ -6,18 +6,37 @@ import arrow from "../../assets/icons/chevron-down.svg";
 
 export default function Navigation() {
   const [drop, setDrop] = useState(false);
-  const dropStyles = [styles.drop, drop ? styles.active : ""];
+  const [menu, setMenu] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+  const setWindowDimensions = () => {
+    setWindowWidth(window.innerWidth);
+    setWindowHeight(window.innerHeight);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", setWindowDimensions);
+
+    return () => {
+      window.removeEventListener("resize", setWindowDimensions);
+    };
+  }, []);
+
   const dropDownClick = () => {
     setDrop(!drop);
   };
-
+  const burgerClick = () => {
+    setMenu(!menu);
+  };
   return (
     <nav className={styles.nav}>
-      <ul>
+      {windowWidth <= 992 && (
+        <span className={`${styles.burger} ${menu ? styles.show : ""}`} onClick={burgerClick}></span>
+      )}
+      <ul className={`${windowWidth <= 992 ? styles.mobileMenu : ""} ${styles.menu} ${menu ? styles.show : ""}`}>
         <li>
           <Link>Nowosci</Link>
         </li>
-        <li className={dropStyles.join(" ")} onClick={dropDownClick}>
+        <li className={`${styles.drop} ${drop ? styles.active : ""}`} onClick={dropDownClick}>
           <Link>Produkty</Link>
           <span>
             <img src={arrow} alt='dropdown icon' />
