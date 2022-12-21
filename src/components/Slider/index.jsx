058f58d274e9b76c9e4f0e3710856b3317eeaed7
valerii <lvaldev.code@gmail.com>
2022-12-22 00:17:08 +0100

    add rwd slider

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,12 +1,16 @@
-import slide01 from "../../assets/images/slide1-big.jpg";
-import slide02 from "../../assets/images/slide2-big.jpg";
-import slide03 from "../../assets/images/slide3-big.jpg";
+import { Link } from "react-router-dom";
 
 import { Pagination, Navigation, Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
+import arrow from "../../assets/icons/chevron-right.svg";
+import slide01 from "../../assets/images/slide1-big.jpg";
+import slide02 from "../../assets/images/slide2-big.jpg";
+import slide03 from "../../assets/images/slide3-big.jpg";
+
 import "swiper/css";
-import "./styles.css";
+import "./slider.css";
+import styles from "./Slider.module.css";
 
 export default function Slider() {
   return (
@@ -20,35 +24,47 @@ export default function Slider() {
       }}>
       <SwiperSlide>
         <img src={slide01} alt='woman with light hair' />
-        <div className='content'>
-          <h1>o1. </h1>
+        <div className={styles.content}>
+          <div className={styles.title}>o1.</div>
           <h2>Bez kompromisu.</h2>
-          <h2>Idealnie takie jak lubisz</h2>
+          <h2>Idealnie takie jak lubisz.</h2>
           <h3>
             keune <span>design</span>
           </h3>
+          <Link>
+            <b></b>
+            <span></span>
+          </Link>
         </div>
       </SwiperSlide>
       <SwiperSlide>
         <img src={slide02} alt='man in white shirt' />
-        <div className='content'>
-          <h1>o1. </h1>
+        <div className={styles.content}>
+          <div className={styles.title}>o1. </div>
           <h2>Bez kompromisu.</h2>
-          <h2>Idealnie takie jak lubisz</h2>
+          <h2>Idealnie takie jak lubisz.</h2>
           <h3>
             keune <span>design</span>
           </h3>
+          <Link>
+            <b></b>
+            <span></span>
+          </Link>
         </div>
       </SwiperSlide>
       <SwiperSlide>
-        <img src={slide03} alt='man in white shirt' />
-        <div className='content'>
-          <h1>o1. </h1>
+        <img src={slide03} alt='woman with the red hair in the garden' />
+        <div className={styles.content}>
+          <div className={styles.title}>o1. </div>
           <h2>Bez kompromisu.</h2>
-          <h2>Idealnie takie jak lubisz</h2>
+          <h2>Idealnie takie jak lubisz.</h2>
           <h3>
             keune <span>design</span>
           </h3>
+          <Link>
+            <b></b>
+            <span></span>
+          </Link>
         </div>
       </SwiperSlide>
     </Swiper>
